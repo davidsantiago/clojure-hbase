e8945d7913984f693eee952edcd0d9418c0f108d
Jose Antonio Ortega Ruiz <jao@gnu.org>
2011-10-30 23:49:56 +0100

    Accidentally deleted package qualifier restored

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-hbase "0.90.4"
+(defproject org.clojars.strongh/clojure-hbase "0.90.4"
   :description "A convenient Clojure interface to HBase."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.hadoop/hadoop-core "0.20.2-dev"]
