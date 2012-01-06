488c122df379454a7efdf3d155ab0b8431eaa1f8
Homer Strong <homer.strong@gmail.com>
2012-01-06 14:34:54 -0800

    dep updates and version bump

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject org.clojars.strongh/clojure-hbase "0.90.4"
+(defproject org.clojars.strongh/clojure-hbase "0.90.5"
   :description "A convenient Clojure interface to HBase."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                 [org.apache.hbase/hbase "0.90.4"]
+                 [org.apache.hbase/hbase "0.90.5"]
                  [org.apache.zookeeper/zookeeper "3.3.3"]
-                 [log4j/log4j "1.2.15" :exclusions [javax.mail/mail
-                                                    javax.jms/jms
-                                                    com.sun.jdmk/jmxtools
-                                                    com.sun.jmx/jmxri]]
+                 [log4j/log4j "1.2.16"]
                  [commons-logging/commons-logging "1.0.4"]])
