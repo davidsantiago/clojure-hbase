3db93c7f1ef95373dd2512a6fe6245445d8cfa9f
David Santiago <david.santiago@gmail.com>
2012-03-05 02:23:02 -0800

    Update set-config to accept a map argument. Add default-config function to get the default configuration as a map. Update unit tests.

--- src/clojure_hbase/core.clj
+++ src/clojure_hbase/core.clj
@@ -20,21 +20,28 @@
 (def ^{:dynamic true :private true} *db*
   (atom nil))
 
+;; There doesn't appear to be, as far as I can tell, a way to get the current
+;; HBaseConfiguration being used by an HTablePool. Unfortunately, this means
+;; you need to remember and keep track of this yourself, if you want to be
+;; switching them around.
+(defn default-config
+  "Returns the default HBaseConfiguration as a map."
+  []
+  (into {} (HBaseConfiguration/create)))
+
 (defn set-config
-  "resets the *db* atom, so that subsequent calls to htable-pool
+  "Resets the *db* atom, so that subsequent calls to htable-pool
    use the new configuration.
 
-   example: (set-config
-              \"hbase.zookeeper.dns.interface\" \"lo\"
-              \"hbase.zookeeper.quorum\" \"127.0.0.1\")"
-  ([k v & kvs]
-     (let [config-obj (HBaseConfiguration/create)
-           kvs (concat [k v] kvs)]
-       (assert (even? (count kvs)))
-       (doseq [[k v] (partition 2 kvs)]
-         (.set config-obj (name k) v))
-       (swap! *db* (fn [_]
-                     (HTablePool. config-obj Integer/MAX_VALUE))))))
+   Example: (set-config
+              {\"hbase.zookeeper.dns.interface\" \"lo\"
+              :hbase.zookeeper.quorum \"127.0.0.1\"})"
+  [config-map]
+  (let [config-obj (HBaseConfiguration/create)]
+    (doseq [[k v] (seq config-map)]
+      (.set config-obj (name k) (name v)))
+    (swap! *db* (fn [_]
+                  (HTablePool. config-obj Integer/MAX_VALUE)))))
   
 (defn- ^HTablePool htable-pool
   []
