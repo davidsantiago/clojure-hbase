40e7b0c0eb93d9f31724a3f99bbd18246bfd3c82
David Santiago <david.santiago@gmail.com>
2012-04-20 18:40:08 -0700

    Update project.clj for lein2.

--- project.clj
+++ project.clj
@@ -9,14 +9,5 @@
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]
                  [org.clojure/tools.logging "0.2.3"]]
-  :multi-deps {"1.2" [[org.clojure/clojure "1.2.0"]]
-               "1.3" [[org.clojure/clojure "1.3.0"]]
-               :all [[org.apache.hadoop/hadoop-core "0.20.205.0"]
-                     [org.apache.hbase/hbase "0.90.5"]
-                     [org.apache.zookeeper/zookeeper "3.3.2"]
-                     [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
-                                                        javax.jms/jms
-                                                        com.sun.jdmk/jmxtools
-                                                        com.sun.jmx/jmxri]]
-                     [org.clojure/tools.logging "0.2.3"]]}
-  :dev-dependencies [])
+  :profiles {:clojure1.2 [[org.clojure/clojure "1.2.0"]]
+             :clojure1.3 [[org.clojure/clojure "1.3.0"]]})
