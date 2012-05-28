cde3f99ebf4965a45db4f85d00bbc65ecd9a021b
David Santiago <david.santiago@gmail.com>
2012-05-28 03:12:20 -0700

    Remove add-column protocol and handle-get-column, replacing usage with new apply-columns function.

--- src/clojure_hbase/core.clj
+++ src/clojure_hbase/core.clj
@@ -293,28 +293,17 @@
    :row-lock     1    ;; :row-lock <a row lock you've got>
    :use-existing 1})  ;; :use-existing <some Get you've made>
 
-(defprotocol AddColumn
-  (add-column [operation family-bytes qualifier-bytes]))
-
-(extend-protocol AddColumn
-  Get
-  (add-column [get-op family-bytes qualifier-bytes]
-    (.addColumn get-op family-bytes qualifier-bytes))
-  Scan
-  (add-column [scan-op family-bytes qualifier-bytes]
-    (.addColumn scan-op family-bytes qualifier-bytes)))
-
-(defn- handle-get-columns
-  "Handles the case where a get operation has requested columns with the
-   :columns specifier (ie, :columns [:family [:col1 :col2 :col3]]). Returns
-   the get operation, but remember, it is mutable. Note that this function
-   is also used by the bindings for Scan, since they have the same functions."
-  [^Get get-op columns]
+(defn- apply-columns
+  "The first argument should be a function of two arguments: a column family
+   name and a column name. The second argument should be the sequence specified
+   by a :columns [:family [:col1 :col2 :col3...] ...] spec (the outer vector in
+   the preceding example). apply-columns will perform the first argument on
+   every (column-family,column) pair specified in the second argument."
+  [func columns]
   (doseq [column (partition 2 columns)] ;; :family [:cols...] pairs.
     (let [[family qualifiers] column]
       (doseq [q qualifiers]
-        (add-column get-op (to-bytes family) (to-bytes q)))))
-  get-op)
+        (func family q)))))
 
 (defn get*
   "Returns a Get object suitable for performing a get on an HTable. To make
@@ -329,7 +318,9 @@
           :column       (apply #(.addColumn get-op
                                             (to-bytes %1) (to-bytes %2))
                                (second spec))
-          :columns      (handle-get-columns get-op (second spec))
+          :columns      (apply-columns #(.addColumn get-op
+                                                    (to-bytes %1) (to-bytes %2))
+                                       (second spec))
           :family       (.addFamily get-op (to-bytes (second spec)))
           :families     (for [f (second spec)]
                           (.addFamily get-op (to-bytes f)))
@@ -577,7 +568,9 @@
           :column       (apply #(.addColumn scan-op
                                             (to-bytes %1) (to-bytes %2))
                                (second spec))
-          :columns      (handle-get-columns scan-op (second spec))
+          :columns      (apply-columns #(.addColumn scan-op
+                                                    (to-bytes %1) (to-bytes %2))
+                                       (second spec))
           :family       (.addFamily scan-op (to-bytes (second spec)))
           :families     (for [f (second spec)]
                           (.addFamily scan-op (to-bytes f)))
