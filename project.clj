045b958b92c1a7cbe74f6b4f67b5ea8cccb250e6
David Santiago <david.santiago@gmail.com>
2012-11-06 13:25:24 -0800

    Spruce up project.clj.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clojure-hbase "0.90.5-4"
   :description "A convenient Clojure interface to HBase."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.apache.hadoop/hadoop-core "0.20.205.0"]
-                 [org.apache.hbase/hbase "0.90.5"]
-                 [org.apache.zookeeper/zookeeper "3.3.2"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.apache.hadoop/hadoop-core "1.0.3"]
+                 [org.apache.hbase/hbase "0.92.2"]
+                 #_[org.apache.zookeeper/zookeeper "3.3.2"]
                  [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
@@ -12,4 +12,5 @@
   :profiles {:clojure1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :clojure1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :hbase90 {:dependencies [[org.apache.hbase/hbase "0.90.5"]]}
-             :hbase92 {:dependencies [[org.apache.hbase/hbase "0.92.1"]]}})
+             :hbase92 {:dependencies [[org.apache.hbase/hbase "0.92.2"]]}
+             :hbase94 {:dependencies [[org.apache.hbase/hbase "0.94.1"]]}})
