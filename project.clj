4c36a1402e33f6ec791bbb1d6519fa833f13bea8
Jason Bray <jason@cerner.com>
2012-12-10 17:57:05 -0600

    Embedding an HBaseTestingUtility to isolate the core tests.  Removing the old hbase90 profile and adding a CDH4 profile to support the Cloudera distribution.

--- project.clj
+++ project.clj
@@ -1,15 +1,27 @@
 (defproject clojure-hbase "0.92.1"
   :description "A convenient Clojure interface to HBase."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.apache.hadoop/hadoop-core "1.0.3"]
                  [org.apache.hbase/hbase "0.92.2"]
                  [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
                                                     javax.jms/jms
                                                     com.sun.jdmk/jmxtools
                                                     com.sun.jmx/jmxri]]
-                 [org.clojure/tools.logging "0.2.3"]]
+                 [org.clojure/tools.logging "0.2.3"]
+                 [org.apache.hbase/hbase "0.92.2" :classifier "tests" :scope "test"]
+                 [org.apache.hadoop/hadoop-test "1.1.0" :scope "test"]]  
   :profiles {:clojure1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :clojure1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :hbase90 {:dependencies [[org.apache.hbase/hbase "0.90.5"]]}
-             :hbase92 {:dependencies [[org.apache.hbase/hbase "0.92.2"]]}
-             :hbase94 {:dependencies [[org.apache.hbase/hbase "0.94.1"]]}})
+             :hbase92 {:dependencies [[org.apache.hbase/hbase "0.92.2"]
+                                      [org.apache.hbase/hbase "0.92.2" :classifier "tests" :scope "test"]
+                                      [org.apache.hadoop/hadoop-test "1.1.0" :scope "test"]]}
+             :hbase94 {:dependencies [[org.apache.hbase/hbase "0.94.1"]
+                                      [org.apache.hbase/hbase "0.94.1" :classifier "tests" :scope "test"]
+                                      [org.apache.hadoop/hadoop-test "1.1.0" :scope "test"]]}
+             :cdh4 {:dependencies [[org.apache.hbase/hbase "0.92.1-cdh4.1.2"]
+                                   [org.apache.hadoop/hadoop-common "2.0.0-cdh4.1.2"]
+                                   [org.apache.hadoop/hadoop-hdfs "2.0.0-cdh4.1.2"]
+                                   [org.apache.hbase/hbase "0.92.1-cdh4.1.2" :classifier "tests" :scope "test"]
+                                   [org.apache.hadoop/hadoop-common "2.0.0-cdh4.1.2" :classifier "tests" :scope "test"]
+                                   [org.apache.hadoop/hadoop-hdfs "2.0.0-cdh4.1.2" :classifier "tests" :scope "test"]
+                                   [org.apache.hadoop/hadoop-test "2.0.0-mr1-cdh4.1.2" :scope "test"]]
+                    :repositories [["cloudera" "https://repository.cloudera.com/content/groups/public/"]]}})
