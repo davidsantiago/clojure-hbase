54c532d06020bb894f604f2c1bebe3a4ada5b101
David Santiago <david.santiago@gmail.com>
2013-04-14 01:37:04 -0700

    Update project.clj for clojure 1.5.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-hbase "0.92.1"
   :description "A convenient Clojure interface to HBase."
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.apache.hbase/hbase "0.92.2"]
                  [log4j/log4j "1.2.16" :exclusions [javax.mail/mail
                                                     javax.jms/jms
@@ -8,9 +8,11 @@
                                                     com.sun.jmx/jmxri]]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.apache.hbase/hbase "0.92.2" :classifier "tests" :scope "test"]
-                 [org.apache.hadoop/hadoop-test "1.1.0" :scope "test"]]  
+                 [org.apache.hadoop/hadoop-test "1.1.0" :scope "test"]]
   :profiles {:clojure1.2 {:dependencies [[org.clojure/clojure "1.2.0"]]}
              :clojure1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :clojure1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :clojure1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :hbase92 {:dependencies [[org.apache.hbase/hbase "0.92.2"]
                                       [org.apache.hbase/hbase "0.92.2" :classifier "tests" :scope "test"]
                                       [org.apache.hadoop/hadoop-test "1.1.0" :scope "test"]]}
